5281d91ce61e6cbebc793ee2e8f1f8d38fc8fa4a
Daniel Araújo <daniel.araujorn01@gmail.com>
2018-05-02 13:45:21 -0300

    working

--- index.js
+++ index.js
@@ -11,50 +11,57 @@ class Perceptron{
 	this.epochs = epochs
 	this.learn_rate = learn_rate
 
-	this.bias = 0
+	this.biasWeight = Math.random() * 2 - 1
 	this.weights = new Array(x[0].length).fill(0).map(i=>(Math.random() * 2 - 1))
 	
 	}
 
-	get accuracy () {
+	accuracy () {
 		return this.acc/this.samples
 	}
 
 	activation (n) {
 		return n < 0 ? 0 : 1
 	}
-	predictNoActivation(input){
-		return this.weights.reduce((a,b,i)=>a+b*input[i])+this.bias
+	multiply(input){
+		let sum = -this.biasWeight
+		for (var i = input.length - 1; i >= 0; i--) {
+			sum+=(input[i]*this.weights[i])
+		}
+		return sum
 	}
 	predict (input) {
-		return this.activation(this.weights.reduce((a,b,i)=>a+b*input[i])+this.bias)
+		return this.activation(this.multiply(input))
 	}
 
-	fit (epocas) {
+	fit () {
 		return new Promise((resolve)=>{
 			for ( let e = 0; e < this.epochs; e++) { 
 				this.x.forEach((input,i)=>{ 
 					let prediction = this.predict(input) 
 					this.y[i] === prediction ? this.acc += 1 : this.acc -= 1
 					let loss = this.y[i] - prediction
-					this.weights=this.weights.map((item,w)=>item+(loss* input[w] * this.learn_rate))
-					this.bias += loss * this.learn_rate
+					for (var p = this.weights.length - 1; p >= 0; p--) {
+						this.weights[p]+=loss*input[p]*this.learn_rate
+					}
+					this.biasWeight -= loss * this.learn_rate
 					this.samples++
 				})
 
-				// console.log(this.accuracy,e)
+				console.log(this.accuracy(),e)
 
-				if(1-this.accuracy<this.error)
+				if(1-this.accuracy()< this.error)
 					break
 			}
+			console.log(this.weights,this.biasWeight)
 			resolve()
 		})
 	}
 }
 let x = [[1, 1], [0, 1], [1, 0], [0, 0]]
 let y = [1, 1, 1, 0]
-let myPerceptron = new Perceptron(x, y, 10000, .1, 0.005)
+let myPerceptron = new Perceptron(x, y, 50000, .01, 0.05)
 
 myPerceptron.fit().then(()=>{
-	console.log('vai',myPerceptron.predictNoActivation([1,0]),myPerceptron.predictNoActivation([0,1]))
+	console.log('vai',myPerceptron.predict([1,1]),myPerceptron.predict([0,0]))
 })
