f24f15f4924f3260ef9538dce04137f17e724a6e
Daniel Araújo <daniel.araujorn01@gmail.com>
2018-05-06 18:00:54 -0300

    nao converge, achar o erro

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ class Perceptron {
         new Array(hiddenLayersCount)
           .fill(0)
           .map(j =>
-            new Array(i ? hiddenLayersCount + 1 : x[0].length + 1)
+            new Array(i ? hiddenLayersCount : x[0].length + 1)
               .fill(0)
               .map(k => Math.random() * 2 - 1)
           )
@@ -59,16 +59,18 @@ class Perceptron {
     }
     return sum;
   }
-  predict(input, weights) {
-    // console.log("predict", input, weights);
-    return this.activation(this.multiply(input, weights));
+  predict(input) {
+    const feedFoward = this.feedFoward(input, this.layers.length - 1);
+    return this.oWeights.map(item =>
+      this.activation(this.multiply(feedFoward, item))
+    );
   }
 
   feedFoward(input, layerIndex) {
     // mostra as entradad da camada que vc deseja
     let validInput = [...input];
     let tempInput = [];
-    for (let i = 0; i < layerIndex + 1; i++) {
+    for (let i = 0; i <= layerIndex; i++) {
       for (let j = 0; j < this.layers[i].length; j++) {
         tempInput = [
           ...tempInput,
@@ -81,14 +83,15 @@ class Perceptron {
     return validInput;
   }
   oneError(input, output) {
-    // acha o erro relativo a camada
     let feedFoward = this.feedFoward(input, this.layers.length - 1);
 
     let sum = 0;
     for (let outputIndex = 0; outputIndex < output.length; outputIndex++) {
       sum += Math.pow(
         output[outputIndex] -
-          this.predict(feedFoward, this.oWeights[outputIndex]),
+          this.activation(
+            this.multiply(feedFoward, this.oWeights[outputIndex])
+          ),
         2
       );
     }
@@ -98,7 +101,9 @@ class Perceptron {
   errorOutput(input, output, pesos) {
     return (
       output -
-      this.predict(this.feedFoward(input, this.layers.length - 1), pesos)
+      this.activation(
+        this.multiply(this.feedFoward(input, this.layers.length - 1), pesos)
+      )
     );
   }
   totalError() {
@@ -114,72 +119,92 @@ class Perceptron {
     // console.log("total error", sum, this.x.length);
     return sum / this.x.length;
   }
+
   fit() {
     return new Promise(resolve => {
+      console.time("train");
       for (let e = 0; e < this.epochs; e++) {
         for (
           let datasetIndex = 0;
           datasetIndex < this.x.length;
           datasetIndex++
         ) {
+          let inputOutLayer = this.feedFoward(
+            this.x[datasetIndex],
+            this.layers.length - 1
+          );
+          let globalGradient = 0;
+          // console.log("camada output");
           for (let neuronio = 0; neuronio < this.oWeights.length; neuronio++) {
-            for (let peso = 0; peso < this.oWeights[neuronio].length; peso++) {
-              let entradaCamada = this.feedFoward(
-                this.x[datasetIndex],
-                this.layers.length - 1
+            // console.log(inputOutLayer, this.oWeights[neuronio]);
+            let gradiente =
+              this.errorOutput(
+                inputOutLayer,
+                this.y[datasetIndex][neuronio],
+                this.oWeights[neuronio]
+              ) *
+              this.activationDerivative(
+                this.multiply(inputOutLayer, this.oWeights[neuronio])
               );
+            for (let peso = 0; peso < this.oWeights[neuronio].length; peso++) {
               this.oWeights[neuronio][peso] +=
-                this.errorOutput(
-                  entradaCamada,
-                  this.y[datasetIndex],
-                  this.oWeights[neuronio]
-                ) *
-                this.activationDerivative(
-                  this.multiply(entradaCamada, this.oWeights[neuronio])
-                ) *
-                entradaCamada[peso];
+                this.learn_rate * gradiente * inputOutLayer[peso];
+              globalGradient += gradiente * this.oWeights[neuronio][peso];
             }
+            // console.log("gg", globalGradient);
           }
-          //para cada camada oculta backwards
-          // para cada neuronio da camada
-          // para cada peso do neuronio
+          for (
+            let layerIndex = this.layers.length - 1;
+            layerIndex >= 0;
+            layerIndex--
+          ) {
+            let tempGlobalGradient = 0;
+            let inputLayer = this.feedFoward(this.x[datasetIndex], layerIndex);
+            for (
+              let neuronioIndex = 0;
+              neuronioIndex < this.layers[layerIndex].length;
+              neuronioIndex++
+            ) {
+              // console.log(inputLayer);
+              let gradiente =
+                this.activationDerivative(
+                  this.multiply(
+                    inputLayer,
+                    this.layers[layerIndex][neuronioIndex]
+                  )
+                ) * globalGradient;
+              for (
+                let pesoIndex = 0;
+                pesoIndex < this.layers[layerIndex][neuronioIndex].length;
+                pesoIndex++
+              ) {
+                this.layers[layerIndex][neuronioIndex][pesoIndex] +=
+                  this.learn_rate * gradiente * inputLayer[pesoIndex];
+                tempGlobalGradient +=
+                  gradiente * this.layers[layerIndex][neuronioIndex][pesoIndex];
+              }
+            }
+            globalGradient += tempGlobalGradient;
+          }
         }
-        // this.layers[camada][neuronio][peso]+=this.learn_rate*this.oneError(this.x[],this.y[])*this.activationDerivative(this.x[],camada)
-        // for (let inputIndex = 0; inputIndex < this.x.length; inputIndex++) {
-        //   for (let a = 0; a < this.y[0].length; a++) {
-        //     let erro = this.error(this.x[inputIndex], this.y[inputIndex]);
-        //   }
-        // }
-        // this.x.forEach((input, i) => {
-        //   let predic tion = this.predict(input);
-        //   this.y[i] === prediction ? (this.acc += 1) : (this.acc -= 1);
-        //   let loss = this.y[i] - prediction;
-        //   for (var p = this.weights.length - 1; p >= 0; p--) {
-        //     this.weights[p] += loss * input[p] * this.learn_rate;
-        //   }
-        //   this.biasWeight -= loss * this.learn_rate;
-        //   this.samples++;
-        // });
-        // console.log(this.accuracy(), e);
-        // if (1 - this.accuracy() < this.error) break;
-        // console.log(this.oWeights);
       }
+      console.timeEnd("train");
       // console.log(this.layers, this.oWeights);
       resolve();
     });
   }
 }
 let x = [[1, 1], [0, 1], [1, 0], [0, 0]];
-let y = [[1], [1], [1], [0]];
-let myPerceptron = new Perceptron(x, 2, 3, y, 1000, 0.01, 0.05);
+let y = [[0], [1], [1], [0]];
+let myPerceptron = new Perceptron(x, 2, 3, y, 10000, 0.01, 0.05);
 
-myPerceptron.fit().then(() => console.log(myPerceptron.totalError()));
-
-// console.log("totalError", myPerceptron.totalError());
-// myPerceptron.fit().then(() => {
-//   console.log(
-//     "vai"
-//     // myPerceptron.predict([1, 1]),
-//     // myPerceptron.predict([0, 0])
-//   );
-// });
+myPerceptron
+  .fit()
+  .then(() =>
+    console.log(
+      myPerceptron.predict([1, 1]),
+      myPerceptron.predict([1, 0]),
+      myPerceptron.predict([0, 1]),
+      myPerceptron.predict([0, 0])
+    )
+  );
